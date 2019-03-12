a1b1da5c950d608e151484d61b004e3d823baa8c
GeeJayOK <239i086c747q>
2019-03-12 09:47:36 +0200

    3.0

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Enter you string: ");
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
+        String enteredString = scanner.nextLine();
+        System.out.println("Input string: " + enteredString);
 
-        char[] charArray = str.toCharArray();
-        System.out.println("Input string: " + str);
-        ReverseString.reverseString(charArray);
+
+        ReverseString.reverseWords(enteredString);
+        char[] charArray = enteredString.toCharArray();
         String revStr = new String(charArray);
-        System.out.println("Output string: " + revStr);
+        //System.out.println("Output string: " + ReverseString.reverseChars(charArray);
     }
 }
 
--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,11 +1,30 @@
 public class ReverseString {
 
-    public static void reverseString(char chars[]) {
+    public static String reverseWords(String enteredString) {
+
+        String[] arrayOfWords = enteredString.split(" ");
+        String reverseString = "";
+        String reverseWord = "";
+
+        for (int numberOfWordInString = 0; numberOfWordInString < arrayOfWords.length; numberOfWordInString++) {
+            String word = arrayOfWords[numberOfWordInString];
+
+            for (int lengthOfWord = word.length() - 1; lengthOfWord >= 0; lengthOfWord--) {
+                char[] charArrayOfWord = word.toCharArray();
+                reverseChars(charArrayOfWord);
+                reverseWord = new String(charArrayOfWord);
+            }
+            reverseString = reverseString + reverseWord + " ";
+        }
+        return reverseString;
+    }
+
+    public static void reverseChars(char chars[]) {
         int rightPointer = chars.length - 1, leftPointer = 0;
         while (leftPointer < rightPointer) {
-            if (!Character.isAlphabetic(chars[leftPointer]) ^ (!Character.isSpaceChar(chars[leftPointer])))
+            if (!Character.isAlphabetic(chars[leftPointer]))
                 leftPointer++;
-            else if (!Character.isAlphabetic(chars[rightPointer]) ^ (!Character.isSpaceChar(chars[rightPointer])))
+            else if (!Character.isAlphabetic(chars[rightPointer]))
                 rightPointer--;
             else {
                 char tmp = chars[leftPointer];
