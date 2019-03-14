293ff32f4496972abf5c05cf4c759e794d579362
GeeJayOK <jarik.get@gmail.com>
2019-03-14 22:26:52 +0200

    5.1

--- src/com/aimprosoft/ReverseString.java
+++ src/com/aimprosoft/ReverseString.java
@@ -3,22 +3,22 @@ package com.aimprosoft;
 public class ReverseString {
 
     public static void reverseWords(String enteredString) {
+        final String SEPARATOR = " ";
 
-        String[] split = enteredString.split("[\\s,!.]");
+        String[] split = enteredString.split(SEPARATOR);
         StringBuilder out = new StringBuilder(enteredString.length());
         StringBuilder temp = new StringBuilder(enteredString.length());
         for (String s : split) {
-            // reset the length and reuse the tmp StringBuilder
             temp.setLength(0);
-            // append the part the the temporary StringBuilder
             temp.append(s);
-            // append the reversed part to the output StringBuilder
+            char[] chars = s.toCharArray();
+            reverseChars(chars);
+            String reversedWord = new String(chars);
             out.append(temp.reverse());
-            // if the length of the input is longer then the output
-            // we need to add the separator char from the input
-            if (enteredString.length() > out.length()) {
-                out.append(enteredString.charAt(out.length()));
-            }
+
+//            if (enteredString.length() > out.length()) {
+//                out.append(enteredString.charAt(out.length()));
+//            }
         }
 
         /*String[] arrayWords = enteredString.split(" ");
