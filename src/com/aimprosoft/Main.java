075c2033eaa2f0f45ebffb2708d28f44f5f7b7dd
GeeJayOK <239i086c747q>
2019-03-15 10:53:21 +0200

    5.0

--- src/com/aimprosoft/Main.java
+++ src/com/aimprosoft/Main.java
@@ -5,15 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        ReverseString reverseString = new ReverseString();
         System.out.println("Enter you string: ");
         Scanner scanner = new Scanner(System.in);
         String enteredString = scanner.nextLine();
         System.out.println("Input string: " + enteredString);
-        ReverseString.reverseWords(enteredString);
-    }
-
-    public static void printResult(String reverseString) {
-        System.out.println("Output string: " + reverseString);
+        System.out.println("Output string: " + reverseString.splitStringToWords(enteredString));
     }
 }
-
--- src/com/aimprosoft/ReverseString.java
+++ src/com/aimprosoft/ReverseString.java
@@ -2,43 +2,26 @@ package com.aimprosoft;
 
 public class ReverseString {
 
-    public static void reverseWords(String enteredString) {
+    public String splitStringToWords(String enteredString) {
         final String SEPARATOR = " ";
+        String reversedWord;
+        String reversedString;
 
         String[] split = enteredString.split(SEPARATOR);
         StringBuilder out = new StringBuilder(enteredString.length());
         StringBuilder temp = new StringBuilder(enteredString.length());
         for (String s : split) {
-            temp.setLength(0);
             temp.append(s);
             char[] chars = s.toCharArray();
             reverseChars(chars);
-            String reversedWord = new String(chars);
-            out.append(temp.reverse());
-
-//            if (enteredString.length() > out.length()) {
-//                out.append(enteredString.charAt(out.length()));
-//            }
+            reversedWord = new String(chars);
+            out.append(reversedWord + SEPARATOR);
         }
-
-        /*String[] arrayWords = enteredString.split(" ");
-        String reverseString = "";
-        String reverseWord = "";
-
-        for (int counter = 0; counter < arrayWords.length; counter++) {
-            String word = arrayWords[counter];
-
-            for (int lengthWord = word.length() - 1; lengthWord >= 0; lengthWord--) {
-                char[] chars = word.toCharArray();
-                reverseChars(chars);
-                reverseWord = new String(chars);
-            }
-            reverseString = reverseString + reverseWord + " ";
-        }
-        Main.printResult(reverseString);*/
+        reversedString = out.toString();
+        return reversedString;
     }
 
-    public static void reverseChars(char chars[]) {
+    private static void reverseChars(char chars[]) {
         int rightCounter = chars.length - 1, leftCounter = 0;
         while (leftCounter < rightCounter) {
             if (!Character.isAlphabetic(chars[leftCounter]))
