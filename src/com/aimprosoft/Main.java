dd3215221d2f7f618eb11fce066d21ac6dfb8f99
GeeJayOK <239i086c747q>
2019-03-15 11:06:21 +0200

    5.0

--- src/com/aimprosoft/Main.java
+++ src/com/aimprosoft/Main.java
@@ -9,7 +9,9 @@ public class Main {
         System.out.println("Enter you string: ");
         Scanner scanner = new Scanner(System.in);
         String enteredString = scanner.nextLine();
-        System.out.println("Input string: " + enteredString);
-        System.out.println("Output string: " + reverseString.splitStringToWords(enteredString));
+        System.out.println("Input string:");
+        System.out.println(enteredString);
+        System.out.println("Output string:");
+        System.out.println(reverseString.splitStringToWords(enteredString));
     }
 }
