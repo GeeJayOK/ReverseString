528a35d69929d4d0baa3594523b0809da4064250
GeeJayOK <239i086c747q>
2019-03-19 12:59:05 +0200

    add test

---
+++ srcTest/com/aimprosoft/yaroslavGetalo/ReverseStringTest.java
@@ -0,0 +1,13 @@
+package com.aimprosoft.yaroslavGetalo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringTest {
+
+    @Test
+    void splitStringToWords() {
+
+    }
+}
